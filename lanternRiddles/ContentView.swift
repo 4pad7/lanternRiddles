3cdefbbe32311df9227ae62d079621fe0f0bd15a
lin1999 <menghan1117@gmail.com>
2021-03-03 16:52:16 +0800

    Initial Commit

---
+++ lanternRiddles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lanternRiddles
+//
+//  Created by User11 on 2021/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
